52c5bb9f33ca4bc966fc0951b0ab0bfcfa2cd373
Tianyibian <billtianyi@gmail.com>
2025-03-25 23:39:36 -0400

    hi trying to do whats overed in class

--- test/HelloWorldTest.java
+++ test/HelloWorldTest.java
@@ -2,10 +2,16 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.beans.Transient;
+
 public class HelloWorldTest {
 
     @Test
     public void testSayHello() {
         assertEquals("Hello, World!", HelloWorld.sayHello());
+    }
+    @Test 
+    public void testNothing(){
+        assertEquals(1,1);
     }
 }
