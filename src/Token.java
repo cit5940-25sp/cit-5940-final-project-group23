30ba1d9d50250de2c2325934bdab52cb665abfa2
Lisa <lisaxiong213@gmail.com>
2025-04-30 10:20:04 -0500

    implemented Token.java

--- src/Token.java
+++ src/Token.java
@@ -1,4 +1,28 @@
-package PACKAGE_NAME;
-
 public class Token {
+    private final TokenType type;
+    private final String value;
+    private final int line;
+
+    public Token(TokenType type, String value, int line) {
+        this.type = type;
+        this.value = value;
+        this.line = line;
+    }
+
+    public TokenType getType() {
+        return type;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public int getLine() {
+        return line;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Token[%s, '%s', line %d]", type, value, line);
+    }
 }
--- src/TokenType.java
+++ src/TokenType.java
@@ -1,4 +1,38 @@
-package PACKAGE_NAME;
+public enum TokenType {
+    // Keywords
+    VAR, FUNCTION, RETURN, IF, ELIF, ELSE, WHILE, RUN, PRINT, INPUT,
 
-public class TokenType {
+    // Literals
+    IDENTIFIER, NUMBER,
+
+    // Arithmetic Operators
+    PLUS,       // +
+    MINUS,      // -
+    STAR,       // *
+    SLASH,      // /
+    MOD,        // %
+
+    // Relational Operators
+    LESS,       // <
+    LESS_EQUAL, // <=
+    GREATER,    // >
+    GREATER_EQUAL, // >=
+    EQUAL,      // =
+    NOT_EQUAL,  // ~
+
+    // Assignment
+    ASSIGN,     // <-
+
+    // Delimiters
+    COMMA,      // ,
+    SEMICOLON,  // ;
+    LPAREN,     // (
+    RPAREN,     // )
+    LBRACE,     // {
+    RBRACE,     // }
+
+    // Special
+    COMMENT,    // ---
+    EOF,        // end of file
+    ERROR       // error message
 }
