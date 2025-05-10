74721e7e8a39074a7664a45d459c95053629f4e3
Lisa <lisaxiong213@gmail.com>
2025-05-10 14:34:25 -0400

    implemented Builtins; added starter code for Interpreter

--- src/Interpreter.java
+++ src/Interpreter.java
@@ -1,2 +1,17 @@
+import java.util.*;
+
 public class Interpreter {
+    public Interpreter() {
+
+    }
+
+    public int callFunction(String name, List<Integer> args) {
+        // TODO: look up FunctionDeclaration, create Environment, execute and return
+        if (Builtins.isBuiltin(name)) {
+            return Builtins.callFunction(name, args);
+        }
+        return 0;
+    }
+
+    // there will be more functions
 }
