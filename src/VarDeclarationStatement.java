1177bcaa02cf435d07d221bfa9e5a4a4110dba1b
Tianyibian <billtianyi@gmail.com>
2025-05-12 23:58:47 -0400

    comment

--- src/VarDeclarationStatement.java
+++ src/VarDeclarationStatement.java
@@ -4,6 +4,7 @@ import java.util.List;
  * Represents a variable declaration statement (var x <- value, y <- value, ...).
  */
 public class VarDeclarationStatement extends Statement {
+    // multiple x,y, z declared in one line
     private final List<VarDeclarator> declarators;
     
     public VarDeclarationStatement(List<VarDeclarator> declarators, int line) {
